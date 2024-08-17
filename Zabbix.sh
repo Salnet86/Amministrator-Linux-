f51d404262ac39c369a1050b42c1c1b2f761fa87
Tecnologie <dosal86@libero.it>
2024-08-17 11:26:56 +0200

    Update Zabbix.sh
    Installazione Zabbix monitoraggio sistemistico

--- Zabbix.sh
+++ Zabbix.sh
@@ -42,7 +42,7 @@ quit;
 
 #Sul server Zabbix l'host importa lo schema iniziale e i dati. Ti verrà chiesto di inserire la password appena creata.
 
- zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
+ cat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
 
 
 ##Disabilitare l'opzione log_bin_trust_function_creators dopo importazione dello schema del database.
